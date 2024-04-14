bd1176929aa96d988df73a4f89fa6ca33a0c662d
Austin Perrine <akperrine@gmail.com>
2024-04-13 18:42:40 -0700

    implement upload file to gridfs

--- src/routes/image.rs
+++ src/routes/image.rs
@@ -1,36 +1,82 @@
-use std::str::FromStr;
+use std::{
+    fmt::{self, Formatter},
+    io::Write,
+    path::Path as StdPath,
+    str::FromStr,
+};
 
-use anyhow::Result;
 use axum::{
-    body::{Body, BodyDataStream, Bytes},
-    extract::Path,
-    http::{header, response, HeaderMap, HeaderValue, StatusCode},
+    body::Bytes,
+    extract::{Multipart, Path},
+    http::{header, StatusCode},
     response::{IntoResponse, Json},
     routing::{delete, get, post},
-    Error, Extension, Router,
+    Router,
 };
 
 use axum_macros::debug_handler;
-use futures_util::{io::BufWriter, AsyncReadExt};
-use image::{codecs::png::PngEncoder, DynamicImage, ImageEncoder, ImageFormat};
-use mongodb::{
-    bson::{oid::ObjectId, Bson},
-    GridFsBucket,
-};
-use tokio::{fs::File, io::AsyncWriteExt};
-use tokio_util::io::ReaderStream;
+use futures_util::{stream::StreamExt, AsyncWriteExt};
+use futures_util::{AsyncReadExt, TryStreamExt};
+use image::ImageFormat;
+use mongodb::bson::{oid::ObjectId, Bson};
+use serde_json::json;
+use tokio::fs;
 
 use crate::db::get_bucket;
 
 pub fn create_route() -> Router {
     Router::new()
         .route("/images/:id", get(get_image_by_id))
-        .route("/images", post(dummy_fn))
+        .route("/images", post(post_image))
         .route("/images/:id", delete(dummy_fn))
 }
 
 pub async fn dummy_fn() {
     println!("hello world");
+}
+
+#[debug_handler]
+pub async fn post_image(
+    mut multipart: Multipart,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    while let Some(mut field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        // let data = field.bytes().await.unwrap();
+        // let fieldName = field.file_name().unwrap();
+        if let Some(filename) = field.file_name().map(ToString::to_string) {
+            println!(
+                "found file field with name: {}, filename: {}",
+                name, filename
+            );
+
+            let mut data: Vec<u8> = Vec::new();
+            let mut file = std::fs::File::create(StdPath::new(&filename))
+                .map_err(|error| "error opening the file path to write")
+                .unwrap();
+            while let Some(chunk) = field.chunk().await.unwrap() {
+                data.extend_from_slice(&chunk);
+            }
+            file.write_all(&data);
+
+            let img_bytes = fs::read(&filename).await.unwrap();
+            let bucket = get_bucket().await.unwrap();
+            let mut upload_stream = bucket.open_upload_stream(&filename, None);
+            upload_stream.write_all(&img_bytes[..]).await.unwrap();
+            upload_stream.close().await.unwrap();
+
+            match fs::remove_file(&filename).await {
+                Ok(()) => println!("File deleted successfully."),
+                Err(err) => println!("Error deleting file: {}", err),
+            }
+        }
+    }
+    // TODO: Take in multipart/form-data with img bytes and name
+    // TODO: Get the bin data read up into GridFs
+
+    let success_response = Json(json!({
+        "message": "Image successfully loaded"
+    }));
+    Ok((StatusCode::OK, success_response))
 }
 
 #[debug_handler]
@@ -60,6 +106,17 @@ pub async fn get_image_by_id(
     Ok((headers, bytes))
 }
 
+#[derive(Debug)]
+struct ProcessImageError(String);
+
+impl fmt::Display for ProcessImageError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "ProcessImageError: {}", self.0)
+    }
+}
+
+impl std::error::Error for ProcessImageError {}
+
 // let id = ObjectId::from_str("661190f4952cdb96750a4405").expect("Could not convert to ObjectId");
 // let mut buf: Vec<u8> = Vec::new();
 // let mut download_stream = bucket
@@ -77,3 +134,10 @@ pub async fn get_image_by_id(
 // let mut output_file = std::fs::File::create("new_sig.png").expect("Unable to create file");
 // img.write_to(&mut output_file, image::ImageFormat::Png)
 //     .unwrap();
+
+// UPLOAD LOGIC WORKS
+// let img_bytes = fs::read("sig.png").await.unwrap();
+
+// let mut upload_stream = bucket.open_upload_stream(&path, None);
+// upload_stream.write_all(&img_bytes[..]).await.unwrap();
+// upload_stream.close().await.unwrap();
