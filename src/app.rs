f888b3325d6ad5ce4523a67f77730ebcaf8b8080
Austin Perrine <akperrine@gmail.com>
2024-04-14 19:54:13 -0700

    set default body limit to 5MB

--- src/app.rs
+++ src/app.rs
@@ -6,7 +6,7 @@ pub async fn create_app() -> Router {
     Router::new()
         .route("/health_check", get(root))
         .merge(routes::image::create_route())
-    // .layer(DefaultBodyLimit::max(5000))
+        .layer(DefaultBodyLimit::max(5 * 1024 * 1024))
 }
 
 async fn root() -> &'static str {
