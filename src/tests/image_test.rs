93fe180de4443b84f21fdcd3a8a7dc628f9a7d9f
Austin Perrine <akperrine@gmail.com>
2024-05-25 16:23:49 -0700

    check for test of double submit with same name for post

--- src/tests/image_test.rs
+++ src/tests/image_test.rs
@@ -1,9 +1,6 @@
 use std::env::{self};
 
-use axum::extract::Multipart;
-use image::buffer;
-use mongodb::options::ClientOptions;
-use reqwest::{Body, Client, StatusCode};
+use reqwest::{multipart::Form, Body, Client, Response, StatusCode};
 use tokio::{fs::File, io::AsyncReadExt};
 use tokio_util::codec::{BytesCodec, FramedRead};
 
@@ -23,29 +20,29 @@ use crate::{db::get_env_config, tests::setup::use_test_app};
 
 //     assert!(client.is_ok());
 // }
-
-#[tokio::test]
-async fn test_post_get_delete_png() {
+#[cfg(test)]
+#[test]
+fn test_post_get_delete_png() {
     use_test_app(async move {
         println!("{}", env::current_dir().unwrap().display());
-        let test_img = File::open("./src/tests/images/test_img_1.png")
-            .await
-            .map_err(|_| "Cannot find test image")
-            .unwrap();
+        // let test_img = File::open("./src/tests/images/test_img_1.png")
+        //     .await
+        //     .map_err(|_| "Cannot find test image")
+        //     .unwrap();
 
-        // read file body stream
-        let stream = FramedRead::new(test_img, BytesCodec::new());
-        let file_body = Body::wrap_stream(stream);
-        //make form part of a file
-        let some_file = reqwest::multipart::Part::stream(file_body)
-            .file_name("test_img_1.png")
-            .mime_str("image/png")
-            .unwrap();
-        // create multipart form
-        let form = reqwest::multipart::Form::new().part("file", some_file);
+        // // read file body stream
+        // let stream = FramedRead::new(test_img, BytesCodec::new());
+        // let file_body = Body::wrap_stream(stream);
+        // //make form part of a file
+        // let some_file = reqwest::multipart::Part::stream(file_body)
+        //     .file_name("test_img_1.png")
+        //     .mime_str("image/png")
+        //     .unwrap();
+        // // create multipart form
+        // let form = reqwest::multipart::Form::new().part("file", some_file);
+        let form = create_multipart_form().await;
 
         let client = Client::new();
-
         let res = client
             .post("http://localhost:3001/images")
             .multipart(form)
@@ -57,6 +54,61 @@ async fn test_post_get_delete_png() {
         let expected = StatusCode::OK;
         println!("{},{}", status_code, expected);
         assert_eq!(status_code, expected);
-    })
-    .await;
+
+        let form_copy = create_multipart_form().await;
+        let res_two = client
+            .post("http://localhost:3001/images")
+            .multipart(form_copy)
+            .send()
+            .await
+            .unwrap();
+        let status_code = res_two.status();
+        let resp_message = res_two.text().await.unwrap();
+        let expected = StatusCode::BAD_REQUEST;
+        println!("{},{}", status_code, expected);
+        // println!("{}", res_two.text().await.unwrap());
+        assert_eq!(
+            resp_message,
+            "\"image name is already taken. Please choose a unique name\""
+        );
+        assert_eq!(status_code, expected);
+    });
+}
+
+async fn create_multipart_form() -> Form {
+    let test_img = File::open("./src/tests/images/test_img_1.png")
+        .await
+        .map_err(|_| "Cannot find test image")
+        .unwrap();
+
+    // read file body stream
+    let stream = FramedRead::new(test_img, BytesCodec::new());
+    let file_body = Body::wrap_stream(stream);
+    //make form part of a file
+    let some_file = reqwest::multipart::Part::stream(file_body)
+        .file_name("test_img_1.png")
+        .mime_str("image/png")
+        .unwrap();
+    // create multipart form
+    let form = reqwest::multipart::Form::new().part("file", some_file);
+    return form;
+    // // read file body stream
+    // let stream = FramedRead::new(file, BytesCodec::new());
+    // let file_body = Body::wrap_stream(stream);
+    // // make form part of a file
+    // let some_file = reqwest::multipart::Part::stream(file_body)
+    //     .file_name("test_img_1.png")
+    //     .mime_str("image/png")
+    //     .unwrap();
+    // // create multipart form
+    // let form = reqwest::multipart::Form::new().part("file", some_file);
+
+    // let client = Client::new();
+
+    // client
+    //     .post("http://localhost:3001/images")
+    //     .multipart(form)
+    //     .send()
+    //     .await
+    //     .unwrap()
 }
