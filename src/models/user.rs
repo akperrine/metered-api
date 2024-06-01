3618af13b5221a5d9b6f8fcce57ef8d436748a67
Austin Perrine <akperrine@gmail.com>
2024-06-01 08:45:51 -0700

    add create user endpoint to mongo

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,11 +1,9 @@
-use bcrypt::bcrypt;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 use wither::bson::doc;
 use wither::{bson::oid::ObjectId, Model};
 
 #[derive(Debug, Model, Serialize, Deserialize, Validate)]
-#[model(index(keys=r#"doc!{"email": 1}"#, options=r#"doc!{"unique": true}"#r))]
 pub struct User {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
@@ -15,10 +13,20 @@ pub struct User {
     pub password: String,
 }
 
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct PublicUser {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub username: String,
+    #[validate(email)]
+    pub email: String,
+}
+
 impl User {
     pub fn new(username: String, email: String, password: String) -> Self {
         let cost = 10;
         let hashed_password = bcrypt::hash(password, cost).unwrap();
+
         Self {
             id: None,
             username,
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,9 +1,53 @@
-use axum::{routing::get, Router};
+use anyhow::Ok;
+use axum::{
+    body::Body,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use bson::{doc, Document};
+use mongodb::Collection;
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+
+use crate::{db::connection, models::user::User};
 
 pub fn create_route() -> Router {
-    Router::new().route("/users/:id", get(dummy_fn))
+    Router::new()
+        .route("/users/:id", get(dummy_fn))
+        .route("/users/create", post(create_user))
 }
 
 async fn dummy_fn() {
     println!("hello from users");
+}
+
+pub async fn create_user(Json(body): Json<CreateUserBody>) -> impl IntoResponse {
+    let db = connection().await;
+    // let client = Client::with_uri_str("uri")
+    let user = User::new(body.username, body.email, body.password);
+
+    let collection: Collection<User> = db.collection("users");
+    // validate
+    let found = collection
+        .find_one(doc! {"email": &user.email}, None)
+        .await
+        .unwrap();
+    println!("found: {:?}", found);
+
+    if let Some(_) = found {
+        // return Json("Username: {} already exists");
+        return (StatusCode::BAD_REQUEST, Json("username already exists"));
+    }
+    let inserted = collection.insert_one(&user, None).await.unwrap();
+    println!("inserted: {:?}", inserted);
+
+    (StatusCode::BAD_REQUEST, Json("user successfully loaded"))
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateUserBody {
+    username: String,
+    email: String,
+    password: String,
 }
