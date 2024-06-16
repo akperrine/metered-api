d76d1de4ce279cfc25a6b8748f59bdb62bf4ea45
Austin Perrine <akperrine@gmail.com>
2024-06-15 17:20:22 -0700

    create a view endpoint that adds a bill and earning per view, still with poor error handling

--- src/routes/file.rs
+++ src/routes/file.rs
@@ -6,9 +6,9 @@ use std::{
 };
 
 use axum::{
-    body::Body,
+    body::{Body, HttpBody},
     extract::{Multipart, Path},
-    http::{header, Response, StatusCode},
+    http::{header, Error, Response, StatusCode},
     response::IntoResponse,
     routing::{delete, get, post},
     Json, Router,
@@ -18,17 +18,17 @@ use bson::{doc, oid::ObjectId, Document};
 use bytes::Bytes;
 use futures::{io::Cursor, TryStreamExt};
 use mime::Mime;
-use mongodb::Collection;
+use mongodb::{Collection, Database};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tokio_util::io::ReaderStream;
 
-use crate::db::connection;
+use crate::{db::connection, models::user::User};
 
 pub fn create_route() -> Router {
     Router::new()
         .route("/files/:id", get(get_file_by_id))
-        .route("/files/view/:file_id", get(get_file_as_view))
+        .route("/files/view", get(get_file_as_view))
         .route("/files/add", post(post_new_file))
         .route("/files/remove", delete(delete_file))
 }
@@ -57,7 +57,49 @@ pub async fn get_file_by_id(
     ))
 }
 
-pub async fn get_file_as_view() {}
+pub async fn get_file_as_view(
+    body: Json<ViewRequestBody>,
+) -> Result<impl IntoResponse, (StatusCode, Vec<u8>)> {
+    let db = connection().await;
+    let mut owner = find_query(&db, &body.owner_id).await.unwrap();
+    let mut viewer = find_query(&db, &body.viewer_id).await.unwrap();
+    println!("{:?}", viewer.account.bill);
+    //get file
+    let collection: Collection<AppFile> = db.collection("files");
+    // messy and needs refactoring... just burning out on project
+    let user_collection: Collection<User> = db.collection("users");
+    let obj_id = ObjectId::from_str(&body.file_id).unwrap();
+    let mut cursor = collection.find(doc! {"_id": obj_id}, None).await.unwrap();
+    while let Some(file) = cursor.try_next().await.unwrap() {
+        let update_owner = user_collection
+            .update_one(
+                doc! {"_id": ObjectId::from_str(&body.owner_id).unwrap()},
+                doc! {"$set": {"account.earnings":  owner.account.earnings + 1}},
+                None,
+            )
+            .await
+            .unwrap();
+        let update_viewer = user_collection
+            .update_one(
+                doc! {"_id": ObjectId::from_str(&body.viewer_id).unwrap()},
+                doc! {"$set": {"account.bill":  viewer.account.bill + 1}},
+                None,
+            )
+            .await
+            .unwrap();
+        println!("{:?}, {:?}", update_owner, update_viewer);
+        // println!("owner: {:?}, viewer: {:?}", owner, viewer);
+        let streamBody = Body::from(file.data);
+        let headers = [(header::CONTENT_TYPE, "application/pdf")];
+
+        return Ok((headers, streamBody));
+    }
+
+    Err(error_fmt(
+        StatusCode::BAD_REQUEST,
+        "Request not processed correctly",
+    ))
+}
 
 pub async fn post_new_file(
     mut multipart: Multipart,
@@ -72,7 +114,7 @@ pub async fn post_new_file(
                 name: field.file_name().unwrap().to_string(),
                 data: field.bytes().await.unwrap().to_vec(),
             };
-
+            // this should check if a file exists with that name first
             let res = collection.insert_one(file_to_insert, None);
 
             println!("{:?}", res.await.unwrap());
@@ -94,9 +136,31 @@ pub struct AppFile {
     pub data: Vec<u8>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ViewRequestBody {
+    file_id: String,
+    owner_id: String,
+    viewer_id: String,
+}
+
 fn error_fmt(status_code: StatusCode, message: &str) -> (StatusCode, Vec<u8>) {
     (
         status_code,
         Json(serde_json::to_vec(&json!({ "message": message })).unwrap()).to_vec(),
     )
+}
+
+async fn find_query(db: &Database, id: &str) -> Result<User, Box<dyn std::error::Error>> {
+    println!("hii, {:?}", id);
+    let collection: Collection<User> = db.collection("users");
+    let obj_id = ObjectId::from_str(id).unwrap();
+    let mut cursor = collection.find(doc! {"_id": obj_id}, None).await.unwrap();
+    while let Some(user) = cursor.try_next().await.unwrap_or_default() {
+        println!("{:?}", user);
+        return Ok(user);
+        // println!("{:?}", &res);
+        // let streamBody = Body::from(file.data);
+        // let headers = [(header::CONTENT_TYPE, "application/pdf")];
+    }
+    Err("User not found")?
 }
